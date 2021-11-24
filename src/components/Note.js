588a913e9d577d71e5526103088f5b323ebcb670
Augustine Cobbold <augustinecobbold6@gmail.com>
2021-11-24 15:18:12 +0000

    timestamp

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
-import {deleteNote} from "../actions/noteActions"
+import { deleteNote } from "../actions/noteActions";
 
 function Note(props) {
+  var today = new Date();
+  var date =
+  today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
+  var time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date + " " + time;
+
   return (
     <div>
       <h4>{props.note.title}</h4>
       <p>{props.note.text}</p>
-      <button onClick={() => {console.log("eidt")}}>Edit</button>
-      <button onClick={() => props.deleteNote}>Delete</button>
+      <p>{dateTime}</p>
+      <button
+        onClick={() => {
+          console.log("eidt");
+        }}
+      >
+        Edit
+      </button>
+      <button onClick={() => props.deleteNote(props.note.id)}>Delete</button>
     </div>
   );
 }
